e0e290f1c6be925fcef9101c6cd2d5c5d092efd6
agungdhewe <agung_dhewe@yahoo.com>
2024-06-15 07:18:26 +0700

    add comment and modify error reporting

--- cache.go
+++ cache.go
@@ -1,10 +1,18 @@
 package dwtpl
 
 import (
+	"fmt"
 	"path/filepath"
 	"text/template"
 )
 
+// CachePages caches the pages in the specified directory by parsing the page templates and storing them in the cache.
+//
+// Parameters:
+// - dir: The directory where the pages are located.
+//
+// Returns:
+// - err: An error if there was a problem caching the pages.
 func (mgr *TemplateManager) CachePages(dir string) (err error) {
 	var pages []string
 
@@ -14,16 +22,16 @@ func (mgr *TemplateManager) CachePages(dir string) (err error) {
 	pattern := filepath.Join(mgr.pagesDirLocation, "*")
 	pages, err = filepath.Glob(pattern)
 	if err != nil {
-		report_error("tidak dapat mengambil data pattern %s", pattern)
-		return err
+		report_error(err.Error())
+		return fmt.Errorf("tidak dapat mengambil data pattern %s", pattern)
 	}
 
 	for _, pagedir := range pages {
 		pagename := filepath.Base(pagedir)
 		tpldata, ispage, err := mgr.ParsePageTemplate(pagedir)
 		if err != nil {
-			report_error("error saat parse halaman %s", pagename)
-			return err
+			report_error(err.Error())
+			return fmt.Errorf("tidak dapat parse halaman %s", pagename)
 		}
 
 		if ispage {
@@ -34,6 +42,15 @@ func (mgr *TemplateManager) CachePages(dir string) (err error) {
 	return nil
 }
 
+// GetCachedPage retrieves a cached page template for the specified page name and device type.
+//
+// Parameters:
+// - pagename: The name of the page to retrieve the template for.
+// - device: The device type for which to retrieve the template.
+//
+// Returns:
+// - tpl: The cached page template, or nil if not found.
+// - err: A boolean indicating if an error occurred while retrieving the template.
 func (mgr *TemplateManager) GetCachedPage(pagename string, device DeviceType) (tpl *template.Template, err bool) {
 	pagedata, pageinmap := mgr.cachedata[pagename]
 	if !pageinmap {
